27622090e0f6ddb64404d9ca355fd378c4a1b624
Zach Latta <zach@zachlatta.com>
2014-01-13 03:07:06 -0800

    Add battery life sample.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$('#batteryLevel').knob({
+    'readOnly': true,
+    'value': 100
+});
+
+var i = 100;
+
+function updateBatteryLife() {
+    if (i > 0) {
+      i--;
+    }
+
+    $('#batteryLevel').val(i).trigger('change');
+
+    setTimeout(updateBatteryLife, 10000);
+}
+
+updateBatteryLife();
